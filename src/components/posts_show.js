cdc2ffb06db171f605fb8f5ee0b60e118112b2c4
agatta1504 <agatta1504@gmail.com>
2017-02-12 17:28:01 +0000

    first commit

---
+++ src/components/posts_show.js
@@ -0,0 +1,39 @@
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import {fetchPost, deletePost} from '../actions/index'
+import {Link} from 'react-router'
+
+class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id)
+  }
+  onDeleteClick = (props) => {
+    this.props.deletePost(this.props.params.id)
+      .then(()=> {
+        this.context.router.push('/')
+      })
+  }
+  render() {
+    const {post} = this.props
+    if(!post) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+        <button className='btn btn-danger pull-xs-right' onClick={this.onDeleteClick}>Delete Post</button>
+        <Link to='/'>Back to Index</Link>
+      </div>
+    )
+  }
+}
+function mapStateToProps(state) {
+  return { post: state.posts.post }
+}
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
